77a947fe0c536dbf81efd67528ced6fc09e9a8e1
TheNotary <no@email.plz>
2026-05-23 11:41:20 -0500

    azd: fixup commit

--- azd/scripts/spec/spec_helper.rb
+++ azd/scripts/spec/spec_helper.rb
@@ -1,8 +1,23 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.max_formatted_output_length = nil # Prevents rspec from truncating diffs
+  end
+  
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.around do |example|
+    original_stdout = $stdout
+    original_stderr = $stderr
+    $stdout = StringIO.new
+    $stderr = StringIO.new
+    example.run
+  ensure
+    $stdout = original_stdout
+    $stderr = original_stderr
   end
 
   config.mock_with :rspec do |mocks|
@@ -10,4 +25,9 @@ RSpec.configure do |config|
   end
 
   config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+  config.filter_run_excluding integration: true
+  config.disable_monkey_patching!
+  config.order = :random
+  Kernel.srand config.seed
 end
